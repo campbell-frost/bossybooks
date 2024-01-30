81600e0e66154da918bb54d0ea7d915d191ed54b
campbell-frost <campbellsfrost@gmail.com>
2024-01-29 20:20:53 -0500

    updated home page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -25,11 +25,8 @@ export default async function Page() {
                     <CardWrapper />
                 </Suspense>
             </div>
-            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <Suspense fallback={<RevenueChartSkeleton />}>
-                    <RevenueChart />
-                </Suspense>
-                <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <div className="mt-6 grid grid-cols-2 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense  fallback={<LatestInvoicesSkeleton />}>
                     <LatestInvoices />
                 </Suspense>
             </div>
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -7,12 +7,11 @@ import { fetchLatestInvoices } from '@/app/lib/data';
 export default async function LatestInvoices(){
 const latestInvoices = await fetchLatestInvoices();
   return (
-    <div className="flex w-full flex-col md:col-span-4">
+    <div className="flex w-full flex-col md:col-span-8">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
 
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
