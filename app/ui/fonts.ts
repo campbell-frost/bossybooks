7a86c781acfc9889c6d703b909cf4e588e01200e
campbell-frost <campbellsfrost@gmail.com>
2024-01-29 20:16:33 -0500

    updated styling

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,14 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google'
+import { Lobster } from 'next/font/google'
 
 export const inter = Inter({ subsets: ['latin'] });
+
+export const lobster = Lobster({
+    weight: ['400'],
+    subsets: ['latin']
+});
+
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin']
