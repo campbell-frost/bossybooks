cb670d843471c10763e42df0c97cd0fcf64d7c83
campbell-frost <campbellsfrost@gmail.com>
2024-02-02 18:03:20 -0500

    made changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-import { lobster } from '@/app/ui/fonts'
+import { inter } from '@/app/ui/fonts'
 
 export default function Page() {
   return (
@@ -17,7 +17,7 @@ export default function Page() {
       </div>
       <div className="">
         <div className="mt-4 grow md:flex-rowflex justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10  md:px-20">
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${inter.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Bossy Books.</strong> 
           </p>
           <Link
