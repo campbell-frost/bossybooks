6071946dac1a96e6a83b1c56538fdee0c6684153
Bribear18 <carmich@g.clemson.edu>
2024-02-06 18:39:24 -0500

    change calendar font to 2xl

--- app/dashboard/calendar/page.tsx
+++ app/dashboard/calendar/page.tsx
@@ -4,5 +4,5 @@ import { lusitana } from '@/app/ui/fonts';
 export default function Page() {
 
     
-    return <p className="text-xl">Calendar </p>;
+    return <p className="text-2xl">Calendar </p>;
 }
