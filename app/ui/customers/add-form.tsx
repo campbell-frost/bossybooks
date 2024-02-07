35480cc2f5eaa6219d37df11e75a939da6ebc363
Bribear18 <carmich@g.clemson.edu>
2024-02-06 23:51:39 -0500

    link add form to add cus

--- app/ui/customers/add-form.tsx
+++ app/ui/customers/add-form.tsx
@@ -26,7 +26,7 @@ import { useFormState } from 'react-dom';
     <div className="w-full px-3"></div>
     </div>
     <Link
-        href="/dashboard/customers"
+        href="/dashboard/customers/add"
         className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
       >
         Cancel
