279fdd9d1765b9053ef463e24ad425ec6ace8dc8
Bribear18 <carmich@g.clemson.edu>
2024-02-06 23:31:33 -0500

    update to add form

--- app/ui/customers/add-form.tsx
+++ app/ui/customers/add-form.tsx
@@ -12,6 +12,8 @@ import { useFormState } from 'react-dom';
         <div className=" w-full md: w/1/2 px-3 md:mb-0">
             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name"/>
             First Name
+            <label/>
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Enter first name"></input>
         </div>
     </div>
     <Link
