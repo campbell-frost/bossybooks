0b8b20f771e2494ced06f9b7ffbf2b6ca99e01cb
Bribear18 <carmich@g.clemson.edu>
2024-02-06 23:24:42 -0500

    add customer button in form

--- app/ui/customers/add-form.tsx
+++ app/ui/customers/add-form.tsx
@@ -14,4 +14,11 @@ import { useFormState } from 'react-dom';
             First Name
         </div>
     </div>
+    <Link
+        href="/dashboard/customers"
+        className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+      >
+        Cancel
+      </Link>
+      <Button type="submit">Add customer</Button>
 </form>
