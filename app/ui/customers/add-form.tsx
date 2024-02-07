0d64799b336a9f1ae8ec1d6f8f2d0dcec2543aeb
Bribear18 <carmich@g.clemson.edu>
2024-02-06 23:21:15 -0500

    form first name

---
+++ app/ui/customers/add-form.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { CustomerField } from '@/app/lib/definitions';
+import Link from 'next/link';
+import { Button } from '@/app/ui/button';
+import { useFormState } from 'react-dom';
+
+
+
+<form className="w-full max-w-lg">
+    <div className="flex-wrap -mx-3 mb-6">
+        <div className=" w-full md: w/1/2 px-3 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name"/>
+            First Name
+        </div>
+    </div>
+</form>
