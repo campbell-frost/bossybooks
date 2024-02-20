09fbecb2d7b7f9faa5e2da579c7a793d0a6fecf5
Bribear18 <carmich@g.clemson.edu>
2024-02-20 00:16:30 -0500

    update cancel size

--- app/ui/customers/add-form.tsx
+++ app/ui/customers/add-form.tsx
@@ -35,7 +35,7 @@ return (
     <Link
         href="/dashboard/customers/
         "
-        className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-200 transition-colors hover:bg-gray-200"
+        className="flex h-10 items-center rounded-sm bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
       >
         Cancel
       </Link>
