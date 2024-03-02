e173ccfff92eed3a5ccdaae724d7cc7557d0fbb9
campbell-frost <campbellsfrost@gmail.com>
2024-03-02 13:04:56 -0500

    Restyled add customer page

--- app/ui/customers/add-form.tsx
+++ app/ui/customers/add-form.tsx
@@ -7,49 +7,47 @@ import { useFormState } from 'react-dom';
 import { addCustomer } from '@/app/lib/actions';
 
 
-  export default function AddCustomerForm({customers}: {customers: CustomerField[]}){
+export default function AddCustomerForm({ customers }: { customers: CustomerField[] }) {
 
-    const initialState = { message: null, errors: {} };
+  const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(addCustomer, initialState);
 
 
-return (<form action={dispatch}>
-<div className="w-full max-w-lg">
-    <div className="flex-wrap -mx-3 mb-6">
-        <div className=" w-full md: w/1/2 px-3 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-extrabold mb-2"/>
-            Customer Name
-            <label/>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:border-bg-pink-600" 
-            id="name"
-             type="name1"
-              placeholder="Enter customer full name"></input>
-        </div>
-  </div>
-  <div className="flex flex-wrap -mx-3 mb-6">
-    <div className="w-full md: w/1/2 px-3 md: mb-0">
-    <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2">
-        Email 
-      </label>
-      <input 
-      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none  focus:border-bg-pink-600" 
-      id="email"
-       type="email1" 
-       placeholder="Enter email address"/>
-    </div>
-    </div>
+  return (<form action={dispatch}>
+    <div className="rounded-md bg-gray-50 p-4 md:p-6">
+      {/* Customer Name */}
+      <div className="mb-6">
+        <label className="mb-2 block text-sm font-medium" />
+        Customer Name
+        <label />
+        <input className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none "
+          id="name"
+          type="name1"
+          placeholder="Enter customer full name"></input>
+
+      </div>
+      <div>
+        <label className="mb-2 block text-sm font-medium">
+          Email
+        </label>
+        <input
+          className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none  focus:border-bg-pink-600"
+          id="email"
+          type="email1"
+          placeholder="Enter email address" />
+      </div>
+    </div >
+
     <div className="mt-6 flex justify-end gap-4">
-    <Link
-        href="/dashboard/customers/
-        "
-        className="flex h-10 items-center rounded-sm bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+      <Link
+        href="/dashboard/customers/"
+        className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
       >
         Cancel
       </Link>
-  <Button type="submit">Add Customer </Button>
-  </div>
-  </div>
-  </form>
-  
-);
+      <Button type="submit">Add Customer </Button>
+    </div>
+  </form >
+
+  );
 }
