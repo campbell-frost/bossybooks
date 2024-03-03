b473ea4b52e5cd539d1012e094dafb9905b61594
campbell-frost <campbellsfrost@gmail.com>
2024-03-03 18:48:41 -0500

    restyled add customer page

--- app/ui/customers/add-form.tsx
+++ app/ui/customers/add-form.tsx
@@ -14,42 +14,50 @@ export default function AddCustomerForm({ customers }: { customers: CustomerFiel
 
 
   return (<form action={dispatch}>
-    <div className="w-full max-w-lg">
-      <div className="flex-wrap -mx-3 mb-6">
-        <div className=" w-full md: w/1/2 px-3 md:mb-0">
-          <label className="block uppercase tracking-wide text-gray-700 text-xs font-extrabold mb-2" />
-          Customer Name
-          <label />
-          <input className="appearance-none block w-full gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:border-bg-pink-600"
-            id="name"
-            type="name"
-            name="name"
-            placeholder="Enter customer full name"></input>
-        </div>
+    <div className="rounded-md bg-gray-50 p-4 md:p-6">
+
+      {/*Customer Name */}
+      <div className="mb-2">
+
+        <label className="mb-4 block text-sm font-medium" />
+        Customer Name
+        <label />
       </div>
-      <div className="flex flex-wrap -mx-3 mb-6">
-        <div className="w-full md: w/1/2 px-3 md: mb-0">
-          <label className="block uppercase tracking-wide text-gray-700 text-xs font-medium mb-2">
-            Email
-          </label>
-          <input
-            className="appearance-none block w-full gray-200 text-gray-700 border border-gray-200 rounded py-2 px-4 leading-tight focus:outline-none  focus:border-bg-pink-600"
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Enter email address" />
-        </div>
+      <div className="relative">
+
+        <input className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500"
+          id="name"
+          type="name"
+          name="name"
+          placeholder="Enter customer full name"></input>
       </div>
-      <div className="mt-6 flex justify-end gap-4">
-        <Link
-          href="/dashboard/customers/
+
+      <div className="pt-5">
+
+        {/*Customer Email */}
+        <label className="mb-2 block text-sm ">
+          Email
+        </label>
+      </div>
+      <div className="relative">
+
+        <input
+          className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-5 text-sm outline-2 placeholder:text-gray-500"
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Enter email address" />
+      </div>
+    </div>
+    <div className="mt-6 flex justify-end gap-4">
+      <Link
+        href="/dashboard/customers/
         "
-          className="flex h-10 items-center rounded-sm gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-        >
-          Cancel
-        </Link>
-        <Button type="submit">Add Customer </Button>
-      </div>
+        className="flex h-10 items-center rounded-sm gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+      >
+        Cancel
+      </Link>
+      <Button type="submit">Add Customer </Button>
     </div>
   </form>
 
