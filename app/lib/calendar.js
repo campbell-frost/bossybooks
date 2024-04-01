286307a6d550f96891f0924c47e3f600cc1e6ebb
Bribear18 <carmich@g.clemson.edu>
2024-04-01 01:44:41 -0400

    added calendar

--- app/lib/calendar.js
+++ app/lib/calendar.js
@@ -1,126 +1,99 @@
-export default async function Calendar(){
+import dayjs from "dayjs";
+import React, { useState } from "react";
+import { generateDate, months } from "./util/calendar";
+import cn from "./util/cn";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 
-    
+'use client'
 
-let date = new Date();
-let year = date.getFullYear();
-let month = date.getMonth();
-    
-const day = document.querySelector(".calendar-dates");
- 
-const currdate = document
-    .querySelector(".calendar-current-date");
- 
-const prenexIcons = document
-    .querySelectorAll(".calendar-navigation span");
+export default function Calendar() {
+	const days = ["S", "M", "T", "W", "T", "F", "S"];
+	const currentDate = dayjs();
+	const [today, setToday] = useState(currentDate);
+	const [selectDate, setSelectDate] = useState(currentDate);
+	return (
+		<div className="flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto  h-screen items-center sm:flex-row flex-col">
+			<div className="w-96 h-96 ">
+				<div className="flex justify-between items-center">
+					<h1 className="select-none font-semibold">
+						{months[today.month()]}, {today.year()}
+					</h1>
+					<div className="flex gap-10 items-center ">
+						<GrFormPrevious
+							className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+							onClick={() => {
+								setToday(today.month(today.month() - 1));
+							}}
+						/>
+						<h1
+							className=" cursor-pointer hover:scale-105 transition-all"
+							onClick={() => {
+								setToday(currentDate);
+							}}
+						>
+							Today
+						</h1>
+						<GrFormNext
+							className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+							onClick={() => {
+								setToday(today.month(today.month() + 1));
+							}}
+						/>
+					</div>
+				</div>
+				<div className="grid grid-cols-7 ">
+					{days.map((day, index) => {
+						return (
+							<h1
+								key={index}
+								className="text-sm text-center h-14 w-14 grid place-content-center text-gray-500 select-none"
+							>
+								{day}
+							</h1>
+						);
+					})}
+				</div>
 
-
-    
- 
-// Array of month names
-const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-];
-
-
- 
-// Function to generate the calendar
-const manipulate = () => {
- 
-    // Get the first day of the month
-    let dayone = new Date(year, month, 1).getDay();
- 
-    // Get the last date of the month
-    let lastdate = new Date(year, month + 1, 0).getDate();
- 
-    // Get the day of the last date of the month
-    let dayend = new Date(year, month, lastdate).getDay();
- 
-    // Get the last date of the previous month
-    let monthlastdate = new Date(year, month, 0).getDate();
- 
-    // Variable to store the generated calendar HTML
-    let lit = "";
- 
-    // Loop to add the last dates of the previous month
-    for (let i = dayone; i > 0; i--) {
-        lit +=
-            `<li class="inactive">${monthlastdate - i + 1}</li>`;
-    }
- 
-    // Loop to add the dates of the current month
-    for (let i = 1; i <= lastdate; i++) {
- 
-        // Check if the current date is today
-        let isToday = i === date.getDate()
-            && month === new Date().getMonth()
-            && year === new Date().getFullYear()
-            ? "active"
-            : "";
-        lit += `<li class="${isToday}">${i}</li>`;
-    }
- 
-    // Loop to add the first dates of the next month
-    for (let i = dayend; i < 6; i++) {
-        lit += `<li class="inactive">${i - dayend + 1}</li>`
-    }
- 
-    // Update the text of the current date element 
-    // with the formatted current month and year
-    currdate.innerText = `${months[month]} ${year}`;
- 
-    // update the HTML of the dates element 
-    // with the generated calendar
-    day.innerHTML = lit;
-}
- 
-manipulate();
- 
-// Attach a click event listener to each icon
-prenexIcons.forEach(icon => {
- 
-    // When an icon is clicked
-    icon.addEventListener("click", () => {
- 
-        // Check if the icon is "calendar-prev"
-        // or "calendar-next"
-        month = icon.id === "calendar-prev" ? month - 1 : month + 1;
- 
-        // Check if the month is out of range
-        if (month < 0 || month > 11) {
- 
-            // Set the date to the first day of the 
-            // month with the new year
-            date = new Date(year, month, new Date().getDate());
- 
-            // Set the year to the new year
-            year = date.getFullYear();
- 
-            // Set the month to the new month
-            month = date.getMonth();
-        }
- 
-        else {
- 
-            // Set the date to the current date
-            date = new Date();
-        }
- 
-        // Call the manipulate function to 
-        // update the calendar display
-        manipulate();
-
-    });
-});
+				<div className=" grid grid-cols-7 ">
+					{generateDate(today.month(), today.year()).map(
+						({ date, currentMonth, today }, index) => {
+							return (
+								<div
+									key={index}
+									className="p-2 text-center h-14 grid place-content-center text-sm border-t"
+								>
+									<h1
+										className={cn(
+											currentMonth ? "" : "text-gray-400",
+											today
+												? "bg-red-600 text-white"
+												: "",
+											selectDate
+												.toDate()
+												.toDateString() ===
+												date.toDate().toDateString()
+												? "bg-black text-white"
+												: "",
+											"h-10 w-10 rounded-full grid place-content-center hover:bg-black hover:text-white transition-all cursor-pointer select-none"
+										)}
+										onClick={() => {
+											setSelectDate(date);
+										}}
+									>
+										{date.date()}
+									</h1>
+								</div>
+							);
+						}
+					)}
+				</div>
+			</div>
+			<div className="h-96 w-96 sm:px-5">
+				<h1 className=" font-semibold">
+					Schedule for {selectDate.toDate().toDateString()}
+				</h1>
+				<p className="text-gray-400">No meetings for today.</p>
+			</div>
+		</div>
+	);
 }
--- auth.config.ts
+++ auth.config.ts
@@ -18,4 +18,4 @@ export const authConfig = {
         },
     },
     providers: [], 
-} satisfies NextAuthConfig;
+} satisfies NextAuthConfig; 
